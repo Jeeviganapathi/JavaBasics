d46d3fb36bbca8f52f565e0168e683455ad961f5
G.Jeevitha Priyadarshini <jeeviganapathi@gmail.com>
2024-01-21 17:18:17 -0500

    added Variable and Data types example

---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+
+    public static void  main(String[] args){
+        int integer = 9;
+        char character ='a';
+        String stringValue = "Java programming";
+        float floatValue = 12.45f;
+        double doubleValue = 18.2389229837477382d;
+        long longValue=182937829;
+
+        System.out.println("integer value = " + integer);
+        System.out.println("character value = " + character);
+        System.out.println("String value = " + stringValue);
+        System.out.println("float value = " + floatValue);
+        System.out.println("double value = " + doubleValue);
+        System.out.println("long value = " + longValue);
+    }
+}
